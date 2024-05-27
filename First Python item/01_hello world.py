e954d281698a5038484c260984ddf97e6afee45c
dw-xdy <3115708251@qq.com>
2024-05-27 23:50:23 +0800

    我的Python学习
    今天刚刚开始接触, 都是一些C语言中学习过的内容, 所以我听课很快, 加油, 赶紧抓紧一切时间学习

---
+++ First Python item/01_hello world.py
@@ -0,0 +1,50 @@
+# 现在我开始正式学习Python这门语言, 还是要加油啊!!!!!努力奋斗!
+
+# 从hello world程序开始我的学习!
+# print("hello world")
+# print("hello \"java\"")
+
+
+# print("jack20")
+# print("Hello,jackhello,tom")
+# print("D:pythonchapter02")
+# print("杨过说：小龙女你嫁给我吧")
+
+# print("郭靖说: \'hello\'")           #单引号也可以不使用转义\, 但是我个人认为这种"模棱两可"的东西还是加上吧, 这样最好
+# print("段誉向西行, 他虽不会武功")      #这个"\r"的意义是将输入的光标转到开始的位置, 有一个“覆盖”的作用,
+                                     #但是只是覆盖后面的内容,还是自己进行尝试就行
+
+
+# print("姓名\t年龄\t籍贯\t住址")           #Python语言中的换行是自动进行的, print只要是分开的就会进行一个换行, 然后就不用使用“\n”了
+# print("john\t12\t河北\t北京")            # 下面的代码就是一个很好的例子
+
+
+# print("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京")
+# print("运行")
+
+
+# print("hello jack")
+# print("hello tom")
+
+"""
+print("hello mary")
+print("hello hsp")
+print("hello smith")
+print("hello john")
+print("hello milan")
+"""
+#或者使用""进行多行注释还有''''''也可以进行多行注释
+
+'''
+print("hello smith")
+print("hello john")
+'''
+
+# print("hello milan")
+# 注意不要进行“多行注释嵌套使用”.
+
+
+
+
+print("-100的绝对值是 ", abs(-100))  # 这里使用了abs函数
+print("-100.67的绝对值是 ", abs(-100.67))
---
+++ First Python item/02_variable.py
@@ -0,0 +1,24 @@
+a = 10
+b = 20
+
+c = 2.66
+# print("a的值为:", a, "类型是：", type(a))
+# print("a的值为:", b, "类型是：", type(b))
+# print("a的值为:", c, "类型是：", type(c))
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(a + b)
+
+# print(c)
+
+name = "tom"
+age = 34
+score = 45
+wage = '23'
+
+print(name, age, score, wage)
+
+print(a % b)
